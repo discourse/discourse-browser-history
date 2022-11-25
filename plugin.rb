2a289795f12e69d276577b02b25df665758a2469
David Taylor <david@taylorhq.com>
2022-11-25 16:43:57 +0000

    FIX: Remove register_asset call for `.hbs` files (#2)
    Files in the `assets/javascripts` directory are automatically compiled, so this was introducing a duplicate. In the latest version of Discourse this also triggers a build error

--- plugin.rb
+++ plugin.rb
@@ -8,6 +8,3 @@ DiscoursePluginRegistry.serialized_current_user_fields << "protect_history"
 after_initialize do
   User.register_custom_field_type('protect_history', :boolean)
 end
-
-register_asset "javascripts/connectors/user-custom-preferences/protect-history.hbs"
-register_asset "javascripts/initializers/discourse_history.js.es6"
