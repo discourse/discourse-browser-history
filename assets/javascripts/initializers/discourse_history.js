7cb2372d46ad3a5a6246b9f964558d5c36408a4e
Isaac Janzen <isaac.janzen@discourse.org>
2023-05-26 13:07:22 -0500

    DEV: Use angle bracket syntax

--- assets/javascripts/initializers/discourse_history.js
+++ assets/javascripts/initializers/discourse_history.js
@@ -1,11 +1,11 @@
-import User from 'discourse/models/user';
-import TopicRoute from 'discourse/routes/topic';
+import User from "discourse/models/user";
+import TopicRoute from "discourse/routes/topic";
 
 export default {
   name: "discourse_history",
-  initialize: function() {
-    if(User.currentProp("custom_fields.protect_history") === true){
+  initialize: function () {
+    if (User.currentProp("custom_fields.protect_history") === true) {
       TopicRoute.disableReplaceState = true;
     }
-  }
+  },
 };
